8e966acbd52416bed4a01054b5d4cdc8a86bbf46
Yifan Mo <mo.yifan13@gmail.com>
2016-01-30 17:24:00 -0500

    Add button and checkboxes

--- ResOhm/MainWindow.xaml.cs
+++ ResOhm/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ResOhm
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+ 
+        }
     }
 }
