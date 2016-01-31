5fa61c7aa2fc917898d8bf99d1e236c14643a610
martinlai <martin9874@gmail.com>
2016-01-31 17:38:32 -0500

    Made significant GUI adjustments and set data bindings

--- ResOhm/MainWindow.xaml.cs
+++ ResOhm/MainWindow.xaml.cs
@@ -19,36 +19,65 @@ namespace ResOhm
 
     public partial class MainWindow : Window
     {
-       
-       
+        Dictionary<string, double> resColours = new Dictionary<string, double>();
+        public Dictionary<string, double> ResColours
+        {
+            get
+            {
+                return resColours;
+            }
+        }
+      // Source   http://www.nullskull.com/faq/175/binding-wpf-combobox-items-to-dictionary.aspx
         public MainWindow()
         {
             InitializeComponent();
-            Dictionary<string, double> ResColours = new Dictionary<string, double>();
+           
             string[] colours = { "Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Grey", "White" };
 
             for (int i = 0; i < colours.Length; i++)
             {
-                ResColours.Add(colours[i], i);
+                resColours.Add(colours[i], i);
             }
-            Grid grid = this.Content as Grid;
-            
-            ComboBox colourSelector = new ComboBox();
-            colourSelector.ItemsSource = ResColours;
-            colourSelector.DisplayMemberPath = "Key";
-     
-            object resBandValue = colourSelector.SelectedItem;
+
+       
           
          
-
-            grid.Children.Add(colourSelector);
         }
+   
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            
+            string numOfBands = ((ComboBoxItem)NumOfBands.SelectedItem).Content.ToString();
         }
 
+        private void NumOfBands_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string numOfBands = ((ComboBoxItem)NumOfBands.SelectedItem).Content.ToString();
 
+            switch (numOfBands)
+            {
+                case "4":
+                    Band4.Visibility = Visibility.Visible;
+                    Band5.Visibility = Visibility.Hidden;
+                    Band6.Visibility = Visibility.Hidden;
+                    break;
+                case "5":
+                    Band4.Visibility = Visibility.Visible;
+                    Band5.Visibility = Visibility.Visible;
+                    Band6.Visibility = Visibility.Hidden;
+                    break;
+                case "6":
+                    Band4.Visibility = Visibility.Visible;
+                    Band5.Visibility = Visibility.Visible;
+                    Band6.Visibility = Visibility.Visible;
+                    break;
+                default:
+                    Band4.Visibility = Visibility.Hidden;
+                    Band5.Visibility = Visibility.Hidden;
+                    Band6.Visibility = Visibility.Hidden;
+                    break;
+            }
+       
+        }
     }
 }
