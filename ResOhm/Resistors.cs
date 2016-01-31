196d0b2bfb4935e34213924b9925e24eb6e6f736
martinlai <martin9874@gmail.com>
2016-01-31 00:47:53 -0500

    Finished implementing resistor factory pattern

--- ResOhm/Resistors.cs
+++ ResOhm/Resistors.cs
@@ -3,133 +3,87 @@ using System.Collections.Generic;
 
 public abstract class Resistor
 {
-    private string band1;
-    private string band2;
-    private string multiplier;
-    private double resistorValue;
+    protected double resistance;
+    protected double tolerance;
+    protected double tempCo;
 
-	
-    // Minimum number of bands is 3; constructor forces minimum of these values to be passed to it
-    protected Resistor(string band1, string band2, string multiplier)
+    //Pass in dictionary containing values of resistor bands based on colour
+    public virtual void CalculateValues(Dictionary<string, double> Band)
     {
-        this.band1 = band1;
-        this.band2 = band2;
-        this.multiplier = multiplier;
-        resistorValue = CalculateResistance();
-    }
-	
-    //Can override virtual method when we have more than 3 bands 
-    public virtual double CalculateResistance()
-    {
-       return ( 10*significantFigures[index1] +　significantFigures[index2]　)* Math.Pow(10, significantFigures[index3] );
+        resistance = (Band["one"] + 10*Band["two"])*Math.Pow(10, Band["multiplier"]);
     }
 
-    public double Resistance() 
-    { 
-            return resistorValue;   
-    }
-	
+    public double Resistance{ get { return resistance;  }    }
 }
 
-public class ThreeBandResistor : Resistor {
-	private ThreeBandResistor(string band1, string band2, string multiplier) : base(band1, band2, multiplier)
-    {
-
-    }
-	
-
-	
-}
-//Store code to be moved to interface with GUI
-public class Temp
+class ThreeBandResistor : Resistor
 {
-    protected string[] color = { "Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Gray", "White" };
-    protected int[] significantFigures = new int[10];
-    protected int index1;
-    protected int index2;
-    protected int index3;
+    //internal keyword means that you cannot directly create a ThreeBandResistor without using the factory
+    internal ThreeBandResistor() {}
 
-    Dictionary<string, double> Resistors = new Dictionary<string, double>();
-		
-        // We can just hard code values, no need to create new array to do this
-		for(int i = 0; i<significantFigures.Length; i ++) {
-			significantFigures[i] = i;
-		}
-		
-		for(int j = 0; j<significantFigures.Length; j ++) {
-			Resistors.Add(color[j],significantFigures[j]);
-		}
-		
-    //GUI will take care of error checking (in fact, if we put in a dropdown list or radio button then the user cannot possibly pass in incorrect values
-		try {
-			index1 = (int) Resistors[band1] ;
-			index2 = (int) Resistors[band2] ;
-			index3 = (int) Resistors[multiplier] ;
-		
-			resistorValue = CalculateResistance();
-			//Console.WriteLine("The resistance is {0}", resistorValue);
-		}
-		catch(Exception e) {
-			//Console.WriteLine("Sorry, you entries are not valid");
-		}
-	
 }
 
-public class Demo{
-	static void Main(){
-	double resistance;
-	
-	Console.WriteLine("Please enter the color of first value band: ");
-	string band1 = Console.ReadLine();
-	
-	Console.WriteLine("Please enter the color of second value band: ");
-	string band2 = Console.ReadLine();
-	
-	Console.WriteLine("Please enter the color of multiplier band: ");
-	string multiplier = Console.ReadLine();
-	
-		ThreeBandResistor a = new ThreeBandResistor(band1, band2, multiplier);	
-	
-	resistance = a.Resistance();
-	
-	Console.WriteLine("Resistance: {0} ohms", resistance);
-		
-	}
-	
+class FourBandResistor : Resistor
+{
+    internal FourBandResistor() {}
+    public override void CalculateValues(Dictionary<string, double> Band)
+    {
+        resistance = (Band["one"] + 10*Band["two"] + 100*Band["three"])*Math.Pow(10, Band["multiplier"]);
+        tolerance = Band["tolerance"];
+    }
+
+    public double Tolerance   { get { return tolerance; } }
+
 }
 
-/*public class Resistance {
-	public static void Main() {
-		
-		Dictionary<string, double> Resistors = new Dictionary<string, double>();
-		
-		string[] color = {"Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Gray", "White"};
-		int[] significantFigures = new int[10];
-		double resistorValue;
-		
-		for(int i = 0; i < significantFigures.Length; i ++) {
-			significantFigures[i] = i;
-		}
-		
-		for(int j = 0; j < significantFigures.Length; j ++) {
-			Resistors.Add(color[j],significantFigures[j]);
-			//Console.WriteLine( "color: {0, 6}" + " Figure: {1, 2}", color[j], significantFigures[j] );
-		}
-		
-		
-		Console.WriteLine("Please enter the left most color band: ");
-		string color1 = Console.ReadLine();
-		Console.WriteLine("Please enter the second color band: ");
-		string color2 = Console.ReadLine();
-		try {
-		int x = (int) Resistors[color1] ;
-		int y = (int) Resistors[color2] ;
-		
-			resistorValue = significantFigures[x] * Math.Pow(10, significantFigures[y]);
-	         Console.WriteLine("The resistance is {0}", resistorValue);
-		}
-		catch(Exception e) {
-			Console.WriteLine("Sorry, you entries are not valid");
-		}
-	}
-} */
+class FiveBandResistor : Resistor
+{
+    internal FiveBandResistor() {}
+    public override void CalculateValues(Dictionary<string, double> Band)
+    {
+        resistance = (Band["one"] + 10 * Band["two"] + 100 * Band["three"] * 1000 * Band["four"]) * Math.Pow(10, Band["multiplier"]);
+        tolerance = Band["tolerance"];
+    }
+    public double Tolerance { get { return tolerance; } }
+}
+
+class SixBandResistor : Resistor
+{
+    internal SixBandResistor() {}
+    public override void CalculateValues(Dictionary<string, double> Band)
+    {
+        resistance = (Band["one"] + 10 * Band["two"] + 100 * Band["three"] * 1000 * Band["four"]) * Math.Pow(10, Band["multiplier"]);
+        tolerance = Band["tolerance"];
+    }
+    public double Tolerance { get { return tolerance; } }
+    public double TempCoefficient { get { return tempCo; } }
+}
+
+// Enforces resistor creation in resistor factory
+abstract class ResistorCreator
+{
+    public abstract Resistor CreateResistor(int numOfBands);
+}
+
+// Factory to decide which type of resistor to create based on number of bands
+class ResistorFactory : ResistorCreator
+{
+    public override Resistor CreateResistor(int numOfBands)
+    {
+        switch(numOfBands)
+        {
+            case 3:
+                return new ThreeBandResistor();
+            case 4:
+                return new FourBandResistor();
+            case 5:
+                return new FiveBandResistor();
+            case 6:
+                return new SixBandResistor();
+            default:
+                // If numOfBands was not 3, 4 , 5, 6 there was some error in parameter passing
+                throw new ArgumentException("Invalid number of bands, cannot create resistor with " + numOfBands + " bands", "numOfBands");
+
+        }
+    }
+}
