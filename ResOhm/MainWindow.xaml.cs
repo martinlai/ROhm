373ef35e899fb66ae7ab29c8e562bd8e99f1bb1e
martinlai <martin9874@gmail.com>
2016-01-31 14:01:59 -0500

    Update GUI

--- ResOhm/MainWindow.xaml.cs
+++ ResOhm/MainWindow.xaml.cs
@@ -15,19 +15,38 @@ using System.Windows.Shapes;
 
 namespace ResOhm
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+ 
+
     public partial class MainWindow : Window
     {
+       
+       
         public MainWindow()
         {
             InitializeComponent();
+            Dictionary<string, double> ResColours = new Dictionary<string, double>();
+            string[] colours = { "Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Grey", "White" };
+
+            for (int i = 0; i < colours.Length; i++)
+            {
+                ResColours.Add(colours[i], i);
+            }
+            Grid grid = this.Content as Grid;
+            
+            ComboBox colourSelector = new ComboBox();
+            colourSelector.ItemsSource = ResColours;
+            colourSelector.DisplayMemberPath = "Key";
+     
+            object resBandValue = colourSelector.SelectedItem;
+          
+         
+
+            grid.Children.Add(colourSelector);
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
- 
+            
         }
 
 
